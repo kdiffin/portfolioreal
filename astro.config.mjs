bacb742311d1d3a4bd148ea6751a51b43cf35701
kdiffin <vhiunese@gmail.com>
2024-11-16 20:45:02 +0400

    seo thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import react from "@astrojs/react";
+import { SITE_URL } from "./src/data/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), sitemap(), robotsTxt(), react()],
+  site: SITE_URL,
 });
